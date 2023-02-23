5452c77a54a874996fb988c1a717dea1c3c0238e
Zan Owsley <zanowsley@gmail.com>
2023-02-22 19:22:45 -0700

    finished rationals, starting on sequences

--- rational_numbers.py
+++ rational_numbers.py
@@ -1,26 +1,44 @@
 from math import gcd
 
 
-def make_rat(x, y):
-    g = gcd(x, y)
-    return [x // g, y // g]
+# def make_rat(x, y):
+#     g = gcd(x, y)
+#     return [x // g, y // g]
 
+
+# def numer(x):
+#     return x[0]
+
+
+# def denom(x):
+#     return x[1]
+
+
+# Alternate functional definition for rationals:
+
+def make_rat(x, y):
+    def get(index):
+        if index == 0:
+            return x
+        elif index == 1:
+            return y
+    return get
 
 def numer(x):
-    return x[0]
-
+    return x(0)
 
 def denom(x):
-    return x[1]
-
+    return x(1)
 
 def rat_equal(x, y):
     return numer(x) * denom(y) == numer(y) * denom(x)
 
 
 def add_rat(x, y):
-    xn, xd = x
-    yn, yd = y
+    xn = numer(x)
+    xd = denom(x)
+    yn = numer(y)
+    yd = denom(y)
     return make_rat(xn * yd + yn * xd, xd * yd)
 
 
@@ -35,3 +53,8 @@ def print_rat(x):
 
 a = make_rat(6, 11)
 b = make_rat(5, 3)
+print_rat(a)
+print_rat(b)
+print_rat(add_rat(a, b))
+print_rat(mul_rat(a, b))
+print(rat_equal(a, b))
